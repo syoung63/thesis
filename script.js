55c34bbecb80e27e2af9af29748c0abda726bf49
syoung63 <young40@sheridancollege.ca>
2023-03-30 20:34:27 -0400

    Connected to Bootstrap
    Added a Bootstrap modal using version 4 and hid it upon button click

--- script.js
+++ script.js
@@ -6,5 +6,23 @@ let path = window.location.pathname;
 if (path == "/index.html") {
     console.log("yes");
     let myElement = document.getElementById("highlight");
-    myElement.style.display = "block";  
+    if(myElement){
+        myElement.style.display = "block";
+    }
+}
+
+
+// State 
+let classroomName = '';
+
+let addClassBtn = document.querySelector('#js-add-class');
+
+if (addClassBtn) {
+    addClassBtn.addEventListener('click', function(){
+
+        
+
+
+        $('#addClassModal').modal('hide')
+    })
 }
