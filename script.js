ae93e069b42f863d0c7e245b7183eeebf4a16799
syoung63 <young40@sheridancollege.ca>
2023-04-08 17:09:47 -0400

    Adding Classrooms
    Added ability to input classroom name, grade, number of student, and description and have it display on classrooms screen

--- script.js
+++ script.js
@@ -2,26 +2,34 @@
 // Get the current URL path
 let path = window.location.pathname;
 
-// Check if the current URL path is the About page
+// Display the highlight over which page the user is on in the nav bar
+let selected = document.getElementById("highlight");
+
 if (path == "/index.html") {
-    console.log("yes");
-    let myElement = document.getElementById("highlight");
-    if(myElement){
-        myElement.style.display = "block";
+    if(selected){
+        selected.style.display = "block";
     }
 }
 
 
 // State 
 let classroomName = '';
-
+let classTitleInput = document.getElementById('classTitleInput');
+let classTitle = document.getElementById('classTitle');
+let introContainer = document.getElementById('intro');
+let classHome = document.getElementById('classroomHome');
 let addClassBtn = document.querySelector('#js-add-class');
 
 if (addClassBtn) {
     addClassBtn.addEventListener('click', function(){
+      
+        classroomName = classTitleInput.value;
+        console.log(classroomName);
 
-        
+        introContainer.style.display = 'none';
+        classHome.style.display = 'flex';
 
+        classTitle.textContent = classroomName; 
 
         $('#addClassModal').modal('hide')
     })
