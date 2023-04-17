486ca7b04e747a04d984e2a4290a40a0b4a5ad15
syoung63 <young40@sheridancollege.ca>
2023-04-16 20:15:49 -0400

    Update script.js

--- script.js
+++ script.js
@@ -98,7 +98,7 @@ if(path == "/classroom.html"){
         // Create the link element and append it to the cardsContainer element
         let classLink = document.createElement("a");
         classLink.style.textDecoration = "none";
-        classLink.href = "../classroom.html";
+        classLink.href = "https://syoung63.github.io/thesis/myclassrooms.html";
         cardsContainer.appendChild(classLink);
     
         // Create the class card div element and append it to the link element
